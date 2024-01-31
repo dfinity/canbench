329d62c62b4f1b59728c2494c80a52411dd8e9d1
Islam El-Ashi <islam.elashi@dfinity.org>
2024-01-31 15:15:12 +0100

    .

--- canbench-rs/src/lib.rs
+++ canbench-rs/src/lib.rs
@@ -1,9 +1,9 @@
 //! A module for profiling canisters.
+pub use canbench_macros::bench;
 use candid::CandidType;
 use serde::{Deserialize, Serialize};
 use std::cell::RefCell;
 use std::collections::BTreeMap;
-pub use canbench_macros::bench;
 
 thread_local! {
     static PROFILING: RefCell<BTreeMap<&'static str, Measurement>> = RefCell::new(BTreeMap::new());
