8d7e69e537906886f0bbea1ee9e3c229a06e40d0
Islam El-Ashi <islam.elashi@dfinity.org>
2024-02-01 15:40:59 +0100

    .

--- scripts/ci_run_benchmark.sh
+++ scripts/ci_run_benchmark.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -Eexuo pipefail
 
+# Script that runs `canbench` at a given directory and outputs a comment
+# that is intended to be posted on the pull request.
+
 # Path to run `canbench` from.
 CANISTER_PATH=$1
 
 CANBENCH_OUTPUT=/tmp/canbench_output.txt
+
+# If changed, then other scripts need to be updated as well.
+COMMENT_MESSAGE_PATH=/tmp/canbench_comment_message.txt
 
 cargo install --path ./canbench-bin
 cd "$CANISTER_PATH"
@@ -12,16 +18,18 @@ cd "$CANISTER_PATH"
 canbench --less-verbose >> $CANBENCH_OUTPUT
 if grep -q "(regressed by \|(improved by" "${CANBENCH_OUTPUT}"; then
   echo "Significant performance change detected! ⚠️
-  If the change is expected, run \`canbench --persist\` to save the updated benchmark results." > $CANBENCH_OUTPUT
+  If the change is expected, run \`canbench --persist\` to save the updated benchmark results." > $COMMENT_MESSAGE_PATH
 else
-  echo "No significant performance changes detected ✅" > $CANBENCH_OUTPUT
+  echo "No significant performance changes detected ✅" > $COMMENT_MESSAGE_PATH
 fi
 
 ## Add the output of canbench to the file.
-echo "" >> $CANBENCH_OUTPUT
-echo "\`\`\`" >> $CANBENCH_OUTPUT
-canbench --less-verbose >> $CANBENCH_OUTPUT
-echo "\`\`\`" >> $CANBENCH_OUTPUT
+{
+  echo ""
+  echo "\`\`\`"
+  echo "$CANBENCH_OUTPUT"
+  echo "\`\`\`"
+} >> $COMMENT_MESSAGE_PATH
 
-# Output the file's contents to stdout.
-cat $CANBENCH_OUTPUT
+# Output the comment to stdout.
+cat $COMMENT_MESSAGE_PATH
