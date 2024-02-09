42ff66d19e353b3afe008f3a84c4fd575226767b
Islam El-Ashi <islam.elashi@dfinity.org>
2024-02-09 15:46:26 +0100

    .

--- canbench-bin/tests/tests.rs
+++ canbench-bin/tests/tests.rs
@@ -277,7 +277,7 @@ fn newer_version() {
         .run(|output| {
         assert_err!(
                 output,
-                "canbench is at version 0.1.0 while the results were generated with version 99.0.0. Please upgrade canbench.
+                "canbench is at version 0.1.1 while the results were generated with version 99.0.0. Please upgrade canbench.
 "
             );
         });
