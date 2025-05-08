5905cb6cdcdd3c92aceaaec25142bcdaa89187f0
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-05-08 07:17:19 +0200

    chore: bump versions to 0.1.12

--- canbench-bin/tests/tests.rs
+++ canbench-bin/tests/tests.rs
@@ -276,7 +276,7 @@ fn newer_version() {
         .run(|output| {
         assert_err!(
                 output,
-                "canbench is at version 0.1.11 while the results were generated with version 99.0.0. Please upgrade canbench.
+                "canbench is at version 0.1.12 while the results were generated with version 99.0.0. Please upgrade canbench.
 "
             );
         });
