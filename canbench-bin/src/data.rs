48bb7b6b2318327159f600832fd3726f56e7794e
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-05-13 14:57:31 +0200

    .

--- canbench-bin/src/data.rs
+++ canbench-bin/src/data.rs
@@ -5,9 +5,9 @@ use std::collections::{BTreeMap, BTreeSet};
 pub(crate) struct Entry {
     status: String,
     benchmark: Benchmark,
-    // instructions: Data,
-    // heap_increase: Data,
-    // stable_memory_increase: Data,
+    instructions: Data,
+    heap_increase: Data,
+    stable_memory_increase: Data,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -25,9 +25,10 @@ impl Benchmark {
     }
 }
 
-// pub(crate) struct Data {
-//     value: f64,
-// }
+pub(crate) struct Data {
+    new: Option<u64>,
+    old: Option<u64>,
+}
 
 pub(crate) fn extract(
     new_results: &BTreeMap<String, BenchResult>,
@@ -44,33 +45,67 @@ pub(crate) fn extract(
         let status = if old.is_some() { "" } else { "new" }.to_string();
         let benchmark = Benchmark::new(new_name, None);
         processed.insert(benchmark.clone());
-        results.push(Entry { status, benchmark });
+        let make_data = |extract: fn(&Measurement) -> u64| Data {
+            new: Some(extract(&new_bench.total)),
+            old: old.map(extract),
+        };
+        results.push(Entry {
+            status,
+            benchmark,
+            instructions: make_data(|m| m.instructions),
+            heap_increase: make_data(|m| m.heap_increase),
+            stable_memory_increase: make_data(|m| m.stable_memory_increase),
+        });
 
         // Process scope measurements.
-        for (scope, _new) in new_bench.scopes.iter() {
+        for (scope, new) in new_bench.scopes.iter() {
             let old = old_bench.and_then(|b| b.scopes.get(scope));
             let status = if old.is_some() { "" } else { "new" }.to_string();
             let benchmark = Benchmark::new(new_name, Some(scope));
             processed.insert(benchmark.clone());
-            results.push(Entry { status, benchmark });
+            let make_data = |extract: fn(&Measurement) -> u64| Data {
+                new: Some(extract(new)),
+                old: old.map(extract),
+            };
+            results.push(Entry {
+                status,
+                benchmark,
+                instructions: make_data(|m| m.instructions),
+                heap_increase: make_data(|m| m.heap_increase),
+                stable_memory_increase: make_data(|m| m.stable_memory_increase),
+            });
         }
     }
 
     // Process removed benchmarks.
     for (old_name, old_bench) in old_results {
-        let old = Benchmark::new(old_name, None);
-        if !processed.contains(&old) {
+        let benchmark = Benchmark::new(old_name, None);
+        if !processed.contains(&benchmark) {
+            let make_data = |extract: fn(&Measurement) -> u64| Data {
+                new: None,
+                old: Some(extract(&old_bench.total)),
+            };
             results.push(Entry {
                 status: "removed".to_string(),
-                benchmark: old,
+                benchmark,
+                instructions: make_data(|m| m.instructions),
+                heap_increase: make_data(|m| m.heap_increase),
+                stable_memory_increase: make_data(|m| m.stable_memory_increase),
             });
         }
-        for (scope, _old) in old_bench.scopes.iter() {
-            let old = Benchmark::new(old_name, Some(scope));
-            if !processed.contains(&old) {
+        for (scope, old) in old_bench.scopes.iter() {
+            let benchmark = Benchmark::new(old_name, Some(scope));
+            if !processed.contains(&benchmark) {
+                let make_data = |extract: fn(&Measurement) -> u64| Data {
+                    new: None,
+                    old: Some(extract(old)),
+                };
                 results.push(Entry {
                     status: "removed".to_string(),
-                    benchmark: old,
+                    benchmark,
+                    instructions: make_data(|m| m.instructions),
+                    heap_increase: make_data(|m| m.heap_increase),
+                    stable_memory_increase: make_data(|m| m.stable_memory_increase),
                 });
             }
         }
