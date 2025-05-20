8e43b0a02e8b9772da440413844f2f70077b5d5f
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-05-20 09:36:43 +0200

    .

--- canbench-bin/src/table.rs
+++ canbench-bin/src/table.rs
@@ -165,7 +165,7 @@ mod tests {
         Entry {
             status: "".to_string(),
             benchmark: Benchmark::new(name, None),
-            calls: Values::new(Some(0), None),
+            calls: Values::new(None, None),
             instructions: Values::new(Some(9_000_000), Some(10_000_000)),
             heap_increase: Values::new(Some(0), None),
             stable_memory_increase: Values::new(Some(0), None),
